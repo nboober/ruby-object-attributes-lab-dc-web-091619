54e63d607cd28c85b97f999612abdc9f77a363ad
Nicholas Boober <nickboober92@gmail.com>
2019-09-17 14:44:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,6 +11,16 @@ class Person
     @name = new_name
   end
   
+    def init_job(job)
+    @job = job
+  end
   
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
   
 end
