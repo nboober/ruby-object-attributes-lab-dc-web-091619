dead4e6647012b554773ae795ce7c5cf72f85d2d
Nicholas Boober <nickboober92@gmail.com>
2019-09-17 14:29:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,11 @@
 class Dog
   
   def name(dog_name)
-    @name = dog_name
-    puts @name
+    puts @dog_name
   end
   
-  def name
-    puts @name
-  end
   
   def name=(dog_name)
-    @name = dog_name
+    puts @dog_name
   end
 end
